115d40052b3665540302750200160e22924bd9f9
miguel bautista <230254@fppiramide.com>
2024-02-24 11:24:51 +0100

    enunciado5 actividad 5.2 terminado

--- UD5/TransformacionDocumentos/src/Actividad_5_2/Enunciado5/App.java
+++ UD5/TransformacionDocumentos/src/Actividad_5_2/Enunciado5/App.java
@@ -7,14 +7,19 @@ import org.w3c.dom.NodeList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.util.ArrayList;
 
 public class App {
+
+    static ArrayList<Perro> perrosList = new ArrayList();
     public static void main(String[] args) {
         try{
             File fichero = new File("src/Actividad_5_2/Enunciado5/content.xml");
+            leerFichero(fichero);
         }catch (Exception ex){
             ex.printStackTrace();
         }
+        getPerros();
 
     }
     public static void leerFichero(File fichero){
@@ -25,7 +30,20 @@ public class App {
             document.getDocumentElement().normalize();
             NodeList perros = document.getElementsByTagName("perro");
             for (int i = 0; i< perros.getLength();i++){
+                //obetenemos el elemento i del array de perros
                 Element item = (Element)perros.item(i);
+                String nombre = item.getElementsByTagName("nombre").item(0).getTextContent();
+                int edad = Integer.parseInt(item.getElementsByTagName("edad").item(0).getTextContent());
+                boolean vacunado;
+                if(item.getElementsByTagName("vacunado").item(0).getTextContent().equalsIgnoreCase("si") || item.getElementsByTagName("vacunado").item(0).getTextContent().equalsIgnoreCase("sí")){
+                    vacunado = true;
+                }else{
+                    vacunado = false;
+                }
+                double peso = Double.parseDouble(item.getElementsByTagName("peso").item(0).getTextContent());
+                String colorPelo = item.getElementsByTagName("color_de_pelo").item(0).getTextContent();
+                //cramos el perro obteniendo su objeto y añadiendolo al arrayList
+                addPerro(crearPerro(nombre,edad,vacunado,peso,colorPelo));
 
             }
 
@@ -33,8 +51,17 @@ public class App {
             ex.printStackTrace();
         }
     }
+    public static Perro crearPerro(String nombre,int edad,boolean vacunado,double peso,String colorPelo){
+        Perro perro = new Perro(nombre,edad,vacunado,peso,colorPelo);
+        return  perro;
+    }
     public static void addPerro(Perro perro){
-
+        perrosList.add(perro);
+    }
+    public static void getPerros(){
+        for(Perro p:perrosList){
+            System.out.println(p);
+        }
     }
 
 
